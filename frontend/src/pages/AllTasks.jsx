46b0cf5a4561d31a491ba20c1449f4f05b31da4b
ranahusain <capthussain2point0@gmail.com>
2025-07-07 11:03:41 +0500

    Read All Tasks

--- frontend/src/pages/AllTasks.jsx
+++ frontend/src/pages/AllTasks.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import "../App.css";
+const url = "http://localhost:5000/api/alltodo/";
 const AllTasks = () => {
-  return <div></div>;
+  const [task, setTask] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        console.log(data);
+        setTask(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTasks();
+  }, []);
+  return (
+    <>
+      <div>
+        {task.map((task, index) => (
+          <div key={index} className="task-card">
+            <p className="task-text">{task.task}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default AllTasks;
